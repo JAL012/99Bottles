670571ff307f2c4388418282ff9dab3c5642e6ef
Jheryl Lezama <jheryllezama@jheryllzamasmbp.attlocal.net>
2017-01-22 14:17:09 -0500

    This Branch contains the working method for 99BottlesOfBeer called within the main method, works

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -16,10 +16,43 @@ public class BeerSong {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Ninety_Nine_Bottles_of_Beer(); //Calls Method for Displaying "99 Bottles of Beer" Song
     }
     
     public static void Ninety_Nine_Bottles_of_Beer(){
         
+        //Counter (Initialized To 99 Beers)
+        int beer = 99;
+        
+        //While Loop (Condition: Until There is 1 Beer Left)
+        while (beer != 1) {
+            
+            //First Verse (Default)
+            System.out.println(beer + " bottles of beer on the wall, " + beer + " bottles of beer");
+            
+            //Decrease Beer By One (For each Loop)
+            beer -= 1;
+            
+            //Second Verse (1 Beer Left)
+            if (beer == 1){
+                System.out.println("take one down, pass it around, " + beer + " bottle of beer on the wall.");
+                System.out.println(); //Formatting Output
+            }
+            
+            //Second Verse (Default)
+            else {
+                System.out.println("take one down, pass it around, " + beer + " bottles of beer on the wall.");
+                System.out.println(); //Formatting Output
+            }
+        }
+        
+        //First Verse (1 Beer Left)
+        System.out.println(beer + " bottle of beer on the wall, " + beer + " bottle of beer");
+        
+        //Second Verse (1 Beer Left)
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+        System.out.println(); //Formatting Output
     }
     
 }
